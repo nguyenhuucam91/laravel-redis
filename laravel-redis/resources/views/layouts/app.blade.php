af2ee0f64ce5ec63ba7c42cc37524b12277179d4
camnh <nguyenhuucam91@gmail.com>
2020-12-25 17:07:04 +0700

    Update app.blade laravel

--- laravel-redis/resources/views/layouts/app.blade.php
+++ laravel-redis/resources/views/layouts/app.blade.php
@@ -57,8 +57,7 @@
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     {{-- Edit profile --}}
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                        onclick="event.preventDefault();document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 

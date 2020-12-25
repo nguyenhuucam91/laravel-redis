2e7cdd6b4d5f330071961553d23d4fb5784c4539
camnh <nguyenhuucam91@gmail.com>
2020-12-25 15:01:16 +0700

    Remove redis code

--- laravel-redis/app/Http/Controllers/UserProfileController.php
+++ laravel-redis/app/Http/Controllers/UserProfileController.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\Http\Controllers;
 
@@ -9,41 +9,13 @@ use Illuminate\Support\Facades\Redis;
 
 class UserProfileController extends Controller
 {
-    public function index() 
+    public function index()
     {
-        //get data from cache
-        $user = null;
-        //if key exists, then get data from REdis key
-        if (Redis::exists('user_profile:'.Auth::user()->id)) {
-            $user = Redis::hgetall('user_profile:'.Auth::user()->id);
-        } 
-        // if key is not exist, we try to find user_information associated with authenticated user id
-        else {
-            $user = UserProfile::where(['user_id' => Auth::user()->id])->first();
-            //if there is no user profile, we create a new object and cast to array, 
-            //since redis result returns array
-            if ($user === null) {
-                $user = new UserProfile;
-            } 
-            //if user exist, we set cache for user data for next acccess
-            else {
-                $user = Redis::hmset('user_profile:'. Auth::user()->id, $user->toArray());
-            }
-            $user = $user->toArray();
-        }
-        //push all data to view
-        return view('user.profile.index', compact('user'));
+
     }
 
     public function store(Request $request)
     {
-        $userId = Auth::user()->id;
-        //merge form data which user inputs with authenticated user_id to update
-        $dataToUpdate = array_merge($request->all(), ['user_id' => $userId]);
-        //perform update and create in the database
-        $userProfile = UserProfile::updateOrCreate(['user_id' => $userId], $dataToUpdate);
-        //delete existing cache if has
-        Redis::exists('user_profile:'.Auth::user()->id) ?? Redis::hdel('user_profile:'.Auth::user()->id);
-        return \redirect()->action('UserProfileController@index');
+
     }
 }
